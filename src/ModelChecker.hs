e6703b19ac0d25be60f06cf88990a59d7bb922d9
Daniil Frumin <difrumin@gmail.com>
2012-11-26 13:28:13 +0400

    Cleaning up a little bit

--- src/ModelChecker.hs
+++ src/ModelChecker.hs
@@ -14,14 +14,37 @@ import Control.Monad.State
 instance (Eq a, Eq b) => Eq (Gr a b) where
   (==) = equal
 
+-- | Successors of a node
 succSt :: SS a -> Node -> [Node]
 succSt = suc
 
+-- | Helper functions
+-- The usual liftM2 f won't work here, we need a left-biased version
+orElse, andThen :: Monad m => m Bool -> m Bool -> m Bool
+orElse a b = do
+  r <- a
+  if r 
+    then return True
+    else b
+         
+andThen a b = do
+  r <- a
+  if r  
+    then b
+    else return False
+         
+  
+notM :: Monad m => m Bool -> m Bool
+notM = liftM not
+
+anyM,allM :: Monad m => (a -> m Bool) -> [a] -> m Bool
+anyM p l = foldM (\a b -> return a `orElse` p b) False l
+allM p l = foldM (\a b -> return a `andThen` p b) True l
+
+-- | Recursive memozied verification function
 verifyF :: NCTL -> SS a -> Node -> State (M.Map (Node,NCTL) Bool) Bool
 verifyF f ss s = do
   m <- get
-  --trace ((show f) ++ " @ " ++ (show s)) $ return ()
-  --trace (show m) $ return ()
   case M.lookup (s,f) m of
     Just x -> return x
     Nothing -> do
@@ -30,27 +53,6 @@ verifyF f ss s = do
       m <- get
       put $ M.insert (s,f) r m
       return r
-
--- | Helper functions
--- The usual liftM2 (||) won't work here, we need a left-biased version
-orElse, andThen :: Monad m => m Bool -> m Bool -> m Bool
-orElse a b = do
-  r <- a
-  if r 
-    then return True
-    else b
-  
-notM :: Monad m => m Bool -> m Bool
-notM = liftM not
-
-andThen a b = do
-  r <- a
-  if r  
-    then b
-    else return False
-         
-anyM p l = foldM (\a b -> return a `orElse` p b) False l
-allM p l = foldM (\a b -> return a `andThen` p b) True l
 
 verify :: NCTL -> SS a -> Node -> State (M.Map (Node,NCTL) Bool) Bool
 verify NCTLFalse _ _ = return False
@@ -69,39 +71,10 @@ verify t@(AU f g) ss s = do
   verifyF g ss s `orElse` 
     (verifyF f ss s `andThen` allM (verifyF t ss) (succSt ss s))
 
+-- | Evaluate the nCTL formula on a given statespace
 evalF :: NCTL -> SS a -> Node -> (Bool,M.Map (Node,NCTL) Bool)
 evalF f ss s = runState (verifyF f ss s) M.empty
 
 lfp f a = 
   let fa = iterate f a 
   in head [x | (x,y) <- zip fa (tail fa), x == y]
-
--- verifyF :: (Show a) => (Eq a,Eq b) => Gr [NCTL a b] [NTrans b] -> NCTL a b -> Gr [NCTL a b] [NTrans b]
--- verifyF gr (NCTLFalse) = gr
--- verifyF gr (NCTLAtom a) = gr -- we assume that all node are already tagged with atoms
--- verifyF gr (NCTLNot f) = nmap notf $ verifyF gr f
---   where notf x = if f `elem` x then x else (NCTLNot f):x
--- verifyF gr (NCTLOr f g) = nmap orfg $ verifyF (verifyF gr f) g
---   where orfg x = if (f `elem` x) && (g `elem` x) then (NCTLOr f g):x else x
--- verifyF gr (EX f) = gmap exf grf
---   where 
---     grf = verifyF gr f
---     exf (to,node,labels,from) = 
---       if any ((elem f) . fromJust . lab grf) (suc grf node) then
---                                       (to,node,(EX f):labels,from)
---                                     else (to,node,labels,from)
--- verifyF gr (EU f g) = lfp (checkEU f g) grf
---   where
---     grf = verifyF (verifyF gr g) f
- 
--- verifyF gr _ = error "Unimplemented"
-     
--- checkEU f g grf = gmap eu grf
---   where    
---     checkElem x = (elem g $ fromJust $ lab grf x) || (elem (EU f g) $ fromJust $ lab grf x)
---     eu (to,node,labels,from) =
---       if not ((EU f g) `elem` labels)
---          && (g `elem` labels 
---          || any checkElem (suc grf node)) then
---         (to,node,(EU f g):labels,from)
---       else (to,node,labels,from)
