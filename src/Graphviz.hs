7f7674d137f7657cbbaf53443ec83b2c4015d7e2
Daniil <ohwow@cpan.org>
2013-03-14 15:52:38 +0400

    Generalizing drawing function

--- src/Graphviz.hs
+++ src/Graphviz.hs
@@ -3,22 +3,22 @@ module Graphviz (drawPT) where
 import PetriNet
 import Data.Set (Set)
 import qualified Data.Set as S
-import Data.MultiSet (MultiSet)
-import qualified Data.MultiSet as MS
+import Data.Foldable (Foldable)
+import qualified Data.Foldable as F
 
 showSet :: Show a => Set a -> String
 showSet = concatMap ((++ "; ") . show) . S.toList
           
-showPre :: PTNet -> Trans -> String
+showPre :: (Show p, Foldable n) => Net p n m -> Trans -> String
 showPre (Net {pre=pre}) t =
-  concatMap ((++ " -> \"" ++ show t ++ "\"; ") . show) (MS.toList (pre t))
+  F.concatMap ((++ " -> \"" ++ show t ++ "\"; ") . show) (pre t)
   
-showPost :: PTNet -> Trans -> String
+showPost :: (Show p, Foldable n) => Net p n m -> Trans -> String
 showPost (Net {post=post}) t =
-  concatMap (\x -> "\"" ++ show t ++ "\" -> " ++ show x ++ "; ") (MS.toList (post t))
+  F.concatMap (\x -> "\"" ++ show t ++ "\" -> " ++ show x ++ "; ") (post t)
 
 
-drawPT :: PTNet -> String
+drawPT :: (Show p, Foldable n) => Net p n m -> String
 drawPT net = 
   unlines 
     [ "digraph PT {"
