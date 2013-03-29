a041db04e1b359fcc2f178893187c38caf3a9a66
Daniil <ohwow@cpan.org>
2013-03-29 22:49:07 +0400

    Parsing XML files into a SNets

--- src/NPNTool/XMLReader.hs
+++ src/NPNTool/XMLReader.hs
@@ -1,25 +1,14 @@
+{-# LANGUAGE Arrows #-}
 module NPNTool.XMLReader where
 
 import Control.Arrow
 import Text.XML.HXT.Core
-import Text.XML.HXT.Arrow.Pickle
 import NPNTool.PetriNet
-import NPNTool.PTConstr
+import NPNTool.NPNet
+import NPNTool.NPNConstr
 
-import NPNTool.Graphviz
-
-newtype Place = Place { placeName :: String }
-                
-xpPlace :: PU Place
-xpPlace = xpElem "places" $
-          xpWrap (Place, placeName) $
-          xpAttr "name" xpText
-
-xpTrans :: PU Trans          
-xpTrans = xpElem "transitions" $
-          xpWrap (Trans, name) $
-          xpAttr "name" xpText
-
+type Label = String
+type Variable = String
 
 idToPlace :: String -> PTPlace
 idToPlace = (+1) . read . tail . dropWhile (/= '.')
@@ -27,37 +16,48 @@ idToPlace = (+1) . read . tail . dropWhile (/= '.')
 idToTrans :: String -> Trans
 idToTrans = Trans . ("t" ++ ) . show . (+1) . read . tail . dropWhile (/= '.')
 
-doc = "/Users/dan/projects/nctlmodelchecker/test/My.npnets"
-
-getPlace :: ArrowXml a => a XmlTree (PTConstrM String ())
+getPlace :: ArrowXml a => a XmlTree (NPNConstrM Variable Label ())
 getPlace =   hasName "places"
---         >>> getAttrValue "name"
          >>> arr (const (mkPlace >> return ()))
 
-getTrans :: IOSArrow XmlTree (PTConstrM String ())
+getTrans :: IOSArrow XmlTree (NPNConstrM Variable Label ())
 getTrans =   hasName "transitions"
---         >>> getAttrValue "name"
-         >>> traceMsg 1 "getTrans"
+         >>> getAttrValue "name"
+         >>> traceValue 1 id
          >>> arr (const (mkTrans >> return ()))
 
-getArcPT :: ArrowXml a => a XmlTree (PTConstrM String ())
+getInscr :: ArrowXml a => a XmlTree (Expr Variable Int)
+getInscr =   hasName "inscription"
+         >>> proc inscr -> do
+           ms <- hasName "monoms" <<< getChildren -< inscr
+           var <- getAttrValue "name" <<< hasName "var" <<< getChildren -< ms
+           power <- arr read <<< getAttrValue "power" -< ms
+           returnA -< exprMult (Var var) power
+           
+getArcPT :: ArrowXml a => a XmlTree (NPNConstrM Variable Label ())
 getArcPT =   hasName "arcsPT"
-         >>> getAttrValue "inPlace" &&& getAttrValue "outTransition"
-         >>> arr (\(p,t) -> inT (idToPlace p) (idToTrans t))
+         >>> proc a -> do
+           p <- getAttrValue "inPlace" -< a
+           t <- getAttrValue "outTransition" -< a
+           inscr <- getInscr <<< hasName "inscription" <<< getChildren -< a
+           returnA -< inT (idToPlace p) inscr (idToTrans t)
 
-getArcTP :: ArrowXml a => a XmlTree (PTConstrM String ())
+getArcTP :: ArrowXml a => a XmlTree (NPNConstrM Variable Label ())
 getArcTP =   hasName "arcsTP"
-         >>> getAttrValue "inTransition" &&& getAttrValue "outPlace" 
-         >>> arr (\(t,p) -> outT (idToTrans t) (idToPlace p))
+         >>> proc a -> do
+           t <- getAttrValue "inTransition" -< a
+           p <- getAttrValue "outPlace" -< a
+           inscr <- getInscr <<< hasName "inscription" <<< getChildren -< a
+           returnA -< outT (idToTrans t) inscr (idToPlace p) 
 
-getNet :: IO [PTConstrM String ()]
-getNet = runX $ configSysVars [withTrace 1]
-         >>> readDocument [] doc
-         >>> multi (isElem >>> (getPlace <+> getTrans
-                                <+> getArcPT <+> getArcTP))
+getNetConstr :: FilePath -> IO [NPNConstrM Variable Label ()]
+getNetConstr doc = runX $ configSysVars [withTrace 1]
+                   >>> readDocument [] doc
+                   >>> multi (isElem >>> (getPlace <+> getTrans
+                                          <+> getArcPT <+> getArcTP))
 
-runConstr :: IO PTNet
-runConstr = do
-  constr <- getNet
-  let (_,pt) = run (sequence constr) new
-  return pt
+runConstr :: FilePath -> IO (SNet Variable Label Int)
+runConstr doc = do
+  constr <- getNetConstr doc
+  let (_,sn) = run (sequence constr) new
+  return sn
